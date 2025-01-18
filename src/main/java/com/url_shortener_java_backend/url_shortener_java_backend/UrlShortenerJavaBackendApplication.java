a7bde8acf2490667e42fb71f33d6227f652e5d13
Snehanjan Chatterjee <snehanjan.29@gmail.com>
2025-01-18 14:01:45 +0530

    Uploading initial project files

---
+++ src/main/java/com/url_shortener_java_backend/url_shortener_java_backend/UrlShortenerJavaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.url_shortener_java_backend.url_shortener_java_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlShortenerJavaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlShortenerJavaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/url_shortener_java_backend/url_shortener_java_backend/UrlShortenerJavaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.url_shortener_java_backend.url_shortener_java_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortenerJavaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
