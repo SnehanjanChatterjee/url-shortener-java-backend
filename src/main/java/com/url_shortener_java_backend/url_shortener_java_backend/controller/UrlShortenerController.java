6c2b24401f96fde561cc09a339e62d03d138f415
Snehanjan Chatterjee <snehanjan.29@gmail.com>
2025-01-19 18:47:54 +0530

    @CrossOrigin doesn't allow DELETE Rest Calls. So added CORS configuration in UrlShortenerJavaBackendApplication

--- src/main/java/com/url_shortener_java_backend/url_shortener_java_backend/controller/UrlShortenerController.java
+++ src/main/java/com/url_shortener_java_backend/url_shortener_java_backend/controller/UrlShortenerController.java
@@ -10,11 +10,9 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
-@CrossOrigin
+@RestController
 @RequestMapping("/v1.0/rest/url-shortener")
 public class UrlShortenerController {
 
